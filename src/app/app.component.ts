c6d94e393b716c9f312e739eb651bfb42de6b6f9
sva71 <crockosha@gmail.com>
2021-07-07 07:39:58 +0300

    Layouting ready.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+    playerName: string = 'John Doe'
+    delay: number = 500
+    playerScore: number = 0
+    compScore: number = 0
+
 }
