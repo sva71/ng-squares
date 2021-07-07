cc78cffa8befc91f81bb1ca18293b76e9a10773c
sva71 <crockosha@gmail.com>
2021-07-07 10:51:43 +0300

    Redesign. Bootstrap included.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { SquareComponent } from './square/square.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -12,7 +13,8 @@ import { SquareComponent } from './square/square.component';
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        NgbModule
     ],
     providers: [],
     bootstrap: [AppComponent]
